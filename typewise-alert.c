c72031b1cd10250deef7549d5760e75ec9119acf
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-31 18:50:28 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -16,6 +16,7 @@ BreachType inferBreach(double value, double lowerboundary, double upperboundary)
 
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
 {
+  limittype limit;
   return inferBreach(temperatureInC, limit.lowerLimit[coolingType], limit.upperLimit[coolingType]);
 }
 
