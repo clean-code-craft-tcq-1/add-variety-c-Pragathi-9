95506aa12db64e61ed15753c7a544dfab6b6cb40
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-04-01 12:00:39 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -14,7 +14,7 @@ typedef enum {
 } BreachType;
 
 
-
+const char* breachtype[]= {"NORMAL", " too LOW", "too HIGH"};
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 BreachType inferBreach(double value, double, double);
 
