e831948d86a70007e446844ac0f0bd2ecbf21207
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-04-07 00:42:12 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -14,7 +14,7 @@ typedef enum {
 } BreachType;
 
 
-bool AlertSuccessful;
+
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 BreachType inferBreach(double value, double, double);
 
