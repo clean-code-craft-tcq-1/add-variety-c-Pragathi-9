2d76262f398e89a350df324cada6213b97cc7ba8
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-04-07 13:56:18 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,8 +3,25 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") 
+TEST_CASE("infers the temperature hasn't breached any limits according to given cooling type") 
 {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(checkAndAlert(TO_CONTROLLER, PASSIVE_COOLING, 12) == NORMAL);
+  REQUIRE(checkAndAlert(TO_EMAIL, HI_ACTIVE_COOLING, 50) == TOO_HIGH);
+  REQUIRE(checkAndAlert(TO_CONSOLE, MED_ACTIVE_COOLING, -5) == TOO_LOW);
 }
+
+
+TEST_CASE("infers the temperature has breached the higher limit according to cooling type") 
+{
+  REQUIRE(checkAndAlert(TO_CONTROLLER, PASSIVE_COOLING, 36) == TOO_HIGH);
+  REQUIRE(checkAndAlert(TO_CONSOLE, HI_ACTIVE_COOLING, 50) == TOO_HIGH);
+  REQUIRE(checkAndAlert(TO_EMAIL, MED_ACTIVE_COOLING, 48) == TOO_HIGH);
+}
+
+TEST_CASE("infers the temperature has breached the lower limit according to cooling type") 
+{
+  REQUIRE(checkAndAlert(TO_CONTROLLER, PASSIVE_COOLING, -20) == NORMAL);
+  REQUIRE(checkAndAlert(TO_EMAIL, HI_ACTIVE_COOLING, -5) == TOO_LOW);
+  REQUIRE(checkAndAlert(TO_CONSOLE, MED_ACTIVE_COOLING, -6) == TOO_LOW);
+}
+
